4739c1c14b3e2721afdff6e35d2f45f703ac8825
LMamon <knit-close.0o@icloud.com>
2025-01-01 23:24:42 -0900

    object manager aligned with workflow

--- ObjectManager.py
+++ ObjectManager.py
@@ -1,82 +1,101 @@
 '''Focuses on geometric analysis and computations, like finding contours, 
     moments, centroids, and axes of least/most moment.'''
 
+import os
+import _heapq
+import numpy as np
 import cv2 as cv
 import ImageManager as Im
 
 class ObjectManager:
-    def __init__(self):
-        self.moments = None
-        self.obj_center = None
-        self.contours = None
-        self.image_visual = None
-        self.lst_axis = None
-        self.max_axis = None
-        #self.centroid()
-
-    def centroid(self):#finds centroid of image selected by user
-        self.contours, hierarchy = cv.findContours(self.image_visual, 
-                                                mode= 1,
-                                                method= 2)
+    def __init__(self, binary_mtx=np.load('binary.npy'), output='results'):
+        self.binary_mtx = binary_mtx
+        self.output = output
+        #find of contours of binary matrix
+        self.contours, _ = cv.findContours(self.binary_mtx, 
+                                                cv.RETR_LIST,
+                                                cv.CHAIN_APPROX_NONE)
         if not self.contours:
-            print("no contours found")
+            print("Binary matrix not detected")
             return 
-        cnt = max(self.contours, key=cv.contourArea) #using first contour to calculate moments
-        self.moments = cv.moments(cnt)
-        M = self.moments
         
+        self.component_heap = []
+        os.makedirs(self.output, exist_ok=True)
+
+    def get_area(self, contour): #calculate area from contours
+        return cv.contourArea(contour)
+    
+    def get_moments(self, contour): #calculate moments from contours
+        return cv.moments(contour)
+
+    def get_centroid(self, contour):
+        M = cv.moments(contour)
         #use moments (M) to calculate centroid (C)
         if M["m00"] != 0: #to avoid division by 0
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            self.obj_center = [cx, cy]
-
-    def show_centroid(self): #finds second moments
-         #load image
-        self.image_visual = cv.imread("results/binary.jpg", cv.IMREAD_GRAYSCALE)
-        if self.image_visual is None:
-            print("No binary image found, run '1) Convert image' first \n")
-            return
-        
-        #get centroid and moments
-        self.centroid()
-        if self.obj_center is None:
-            print("No centroid or moments found")
-            return
-        C = self.obj_center
-
-        render = cv.cvtColor(self.image_visual, cv.COLOR_GRAY2BGR)
-        
-        #draw centroid
-        cv.circle(render, C,3, color=(0, 0, 255), thickness=1)
-        cv.putText(render, 
-                f"Centroid: ({C[0]}, {C[1]})", 
-                (C[0]+4, C[1]), 
-                cv.FONT_HERSHEY_PLAIN, 
-                .3,
-                (0,0,255),
-                1,
-                bottomLeftOrigin=False)
-        
-        cv.imshow("Centroid of object", render)
-        print("Click on Window And Press Any Key To Close Image")
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-
-
-    def show_axis(self):
-        cmx = self.moments
-        
-        #("m20") - (self.centroid[0] * self.moments.get("m10"))
-        #cmy = self.moments.get("m02") - (self.centroid[1] * self.moments.get("m01"))
-        #mcm = self.moments.get("m11") - (self.centroid[0] * self.moments.get("m01"))
-        print((cmx), "+")
-
-
-        '''print("\nMoments: ")
-        for key, value in self.moments.items():
-            print(f"{key}: {value}")'''
-
+            return (cx, cy)
     
+    def get_axis(self, contour):
+        M = cv.moments(contour)
+        
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        #get central moments
+        mu20 = M['m20'] - cx * M['m10']
+        mu02 = M['m02'] - cy * M['m01']
+        mu11 = M['m11'] - cx * M['m01'] - cy * M['m10']
 
+        #calculate angle of major axis
+        angle = 0.5 * np.arctan2(2 * mu11, mu20 - mu02)
     
+        #alculate length of the major and minor axes
+        area = M['m00']
+        major_axis_len = np.sqrt((2 * (mu20 + mu02 + np.sqrt((mu20 - mu02)**2 + 4 * mu11**2))) / area)
+        minor_axis_len = np.sqrt((2 * (mu20 + mu02 - np.sqrt((mu20 - mu02)**2 + 4 * mu11**2))) / area)
+
+        return major_axis_len, minor_axis_len, angle
+
+        
+    def gen_components(self): #get relevant information from detected objects
+        for label, contour in enumerate(self.contours, start=1):
+            A = self.get_area(contour)
+            M = self.get_moments(contour)
+            centroid = self.get_centroid(contour)
+            major_axis_len, minor_axis_len, angle = self.get_axis(contour)
+
+            #insert data into heap
+            _heapq.heappush(self.component_heap,(-A, centroid, M, major_axis_len, minor_axis_len, label, angle)) 
+ 
+        self.save_to_text()
+        self.save_to_npy()            
+            
+
+    def save_to_text(self): #save data to .txt file
+        text_path = os.path.join(self.output, 'properties.txt')
+        #write text to file
+        with open(text_path, 'w') as f:
+            for data in self.component_heap:
+                f.write(f'{data[5]}: Area: {-data[0]} px, Centroid: {data[1]}, Angle: {data[6]}\n')
+        print(f'Objects detected printed to: {text_path}')
+    
+    def save_to_npy(self): #save data to .npy for visualization
+        npy_path =os.path.join(self.output, "components.npy")
+        component_data = []
+        for data in self.component_heap:
+            components = {'label': data[5],
+                          'area': -data[0],
+                          'centroid' : data[1],
+                          'moments' : data[2],
+                          'major_axis' : data[3],
+                          'minor_axis' : data[4],
+                          'angle' : data[6]}
+            
+            component_data.append(components)
+
+        np.save(npy_path, component_data)
+        print(f'Componnets saved to: {npy_path}')
+
+
+analysis = ObjectManager()
+analysis.gen_components()

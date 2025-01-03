812ad6fb09ca7a24833debd015a6b82f8825c897
LMamon <knit-close.0o@icloud.com>
2025-01-02 23:58:17 -0900

    fixed + added manager initialization

--- main.py
+++ main.py
@@ -3,29 +3,72 @@
 import sys
 import ImageManager
 import RenderManager as rnd
-
+import ObjectManager as obj
     
 
 def main():#main menu prompt
-    img_mgr = ImageManager.ImageManager() #instanace of image manager
-    rnd_mgr = rnd.RenderManager()    
-    
+    print("\n\t\tWelcome!\n")
 
-    choice = input("\t\tWelcome!\n \n1) Choose image with object(s) \n2) Show binary \
-                   \n3) Show color image \n4) Show centroid of object(s) \
-                   \n5) Show least and max moment Axis\n0) Exit\n>>")
-    options = {
-        '1' : img_mgr.get_image, #get user to select image
-        '2' : rnd_mgr.show_binary,
-        '3' : rnd_mgr.show_color,
-        '4' : rnd_mgr.show_centroid,
+    img_mgr = ImageManager.ImageManager() #instanace of image manager
+    obj_mgr = None
+    rnd_mgr = None
+
+    while True: 
+        print("1) Choose image with object(s)")
+        print("2) Show binary")
+        print("3) Show color")
+        print("4) Show centroid of ojects")
+        print('5) Show least and max moments')
+        print("0) Exit")
+
+        choice = input(">>")
+
+        if choice == '1':
+            img_mgr.get_image()
+            obj_mgr = obj.ObjectManager()
+            rnd_mgr = rnd.RenderManager(obj_mgr)    
         
-        '0' : sys.exit
-    }
-    if choice == '0':
-        print("\n\n\t\tGoodbye")
-    options[choice]()
-    main()
+        elif choice == '2':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                rnd_mgr.show_binary()
+        elif choice == '3':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                rnd_mgr.show_color()
+        elif choice == '4':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                obj_mgr.gen_components()
+                rnd_mgr.show_centroid()
+        elif choice == '5':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                obj_mgr.gen_components()
+                rnd_mgr #place holder
+        elif choice == '6':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                obj_mgr.gen_components()
+                rnd_mgr #place holder
+        elif choice == '7':
+            if rnd_mgr is None:
+                print("please load an image first (option 1)")
+            else:
+                obj_mgr.gen_components()
+                rnd_mgr #place holder
+
+        elif choice == '0':
+            print("\n\tGoodbye")
+            sys.exit()
+
+        else:
+            print("Please input a valid option")
 
 if __name__=="__main__":
     main()

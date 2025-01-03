a6291a163ac8d8583d592f91656c46af5c875f59
LMamon <knit-close.0o@icloud.com>
2025-01-02 17:23:42 -0900

    feat: add image selection and preprocessing

--- ImageManager.py
+++ ImageManager.py
@@ -2,111 +2,73 @@
     and storing images.'''
 
 import os
+
 import numpy as np
 import tkinter as tk
 import cv2 as cv
-
 from tkinter import filedialog
 
 class ImageManager: 
     def __init__(self, output="results"):
         self.output = output
-        self.color_img = None
-        self.color_mtx = None
         self.file_path = None
 
+        self.color_mtx = None
         self.gray_mtx = None
-        
-        self.binary_img = None
         self.binary_mtx = None
-        self.binary_path = None
         
         os.makedirs(self.output, exist_ok=True)
         
+    def convert_to_gray(self): #convert color mtx to grayscale
+        self.gray_mtx = cv.cvtColor(self.color_mtx, cv.IMREAD_GRAYSCALE)
 
-    def get_img(self):#user gets color image and loaded to self.color_matrix
-        root=tk.Tk()
+    def convert_to_binary(self): #convert grayscale to binary using thresholding and invert so objects are white
+        _, gray = cv.threshold(self.gray_mtx, 190, 250, 0)
+        self.binary_mtx = cv.bitwise_not(gray)
+        if self.binary_mtx is None:
+            print("Error converting grayscale image")
+    
+    def save_color_mtx(self): #save color mtx to results for optional visualization 
+        color_path = os.path.join(self.output, "color.npy")
+        np.save(color_path, self.color_mtx)
+    
+    def save_binary_mtx(self):
+        binary_path = os.path.join(self.output, "binary.npy")
+        np.save(binary_path, self.binary_mtx)
+
+    def save_binary_jpg(self): #uses cv to save jpg not np since its no a matrix
+        jpg_path = os.path.join(self.output, "binary.jpg")
+        cv.imwrite(jpg_path, self.binary_mtx)
+        if self.binary_mtx is None:
+            print("Binary image not saved")
+            return
+        
+    def load_color_mtx(self):
+        try:
+            color_path = os.path.join(self.output, "color.npy")
+            return np.load(color_path)
+        except FileNotFoundError:
+            print("No color matrix found")
+    
+    def get_image(self): #user picks image to use
+        root= tk.Tk()
         root.withdraw() #hide root window
         root.attributes("-topmost", True)
-        
-        #open file explorer
-        types = [('JPEG', '*.png'),('JPEG','*.jpeg'), ('PDF','*.pdf'), ('TIFF', '*.tiff')]
-        self.file_path = filedialog.askopenfilename(parent=root, title="Select an image", filetypes=types)
-        if self.file_path is None:
+
+        filetypes = [('JPEG', '*.png'),('JPEG','*.jpeg'), ('PDF','*.pdf'), ('TIFF', '*.tiff')]
+        self.file_path = filedialog.askopenfilename(parent=root, title="Select an image", filetypes=filetypes)
+        if not self.file_path:
             print("No image loaded")
-        root.quit()
-        #small window doesnt close for whatever reason
-        
+            return
+        root.quit() #small window doesnt close for some reason
+
+        self.color_mtx = cv.imread(self.file_path, cv.IMREAD_ANYCOLOR)
         if self.color_mtx is None:
-            self.color_mtx = cv.imread(self.file_path) #sets self.color_img to image in file_path
-            self.save_color_mtx()
-            self.grayscale()
-            if self.color_mtx is None:
-                print("No color image")
-    
-
-    def save_color_mtx(self): #save color image matrix to results folder
-        color_mtx_path = os.path.join(self.output, "color.npy")
-        np.save((color_mtx_path), self.color_mtx)
-        print(f'Color Matrix saved to: {color_mtx_path}')
-    
-    def load_color_mtx(self): #load color image matrix
-        try:
-            color_mtx_path = os.path.join(self.output, "color.npy")
-            self.color_mtx = np.load(color_mtx_path)
-        except FileNotFoundError:
-            print(f'Color matix not found at: {color_mtx_path}')
-
-
-    def save_binary_mtx(self): #save binary matrix to results folder
-        binary_mtx_path = os.path.join(self.output, "binary.npy")
-        if self.binary_mtx is None:
-            print("No binary matrix to save")
+            print("Failed to load color image")
             return
-        np.save((binary_mtx_path), self.binary_mtx)
-        print(f'Binary matrix saved to: {binary_mtx_path}')
-    
-    def load_binary_mtx(self): #load binary matrix
-        try:
-            binary_mtx_path = os.path.join(self.output, "binary.npy")
-            self.binary_mtx = np.load(binary_mtx_path)
-        except FileNotFoundError:
-            print(f'Binary matix not found at: {binary_mtx_path}')
-        
 
-    def grayscale(self): #set.gray_mtx with greyscaled color image
-        if self.color_mtx is None:
-            try:
-                self.load_color_mtx()
-            except Exception:
-                print("Loaded color matrix is empty")
-                return
-        self.gray_mtx = cv.cvtColor(self.color_mtx, cv.COLOR_BGR2GRAY)
-
-
-    def convert(self): #convert grayscale image to binary image and save
-        if self.gray_mtx is None:
-            self.grayscale()
-            if self.gray_mtx is None:
-                print("No grayscale image to apply threshold to")
-                return
-        
-        _,self.binary_mtx = cv.threshold(self.gray_mtx, 230,250, 0) #convert threshold to binary
-        self.binary_mtx = cv.bitwise_not(self.binary_mtx) #invert image, shapes should be white
-
-        if self.binary_mtx is None:
-            print("Error generating binary image")
-            return
-        
-        #create output path for binary_mtx
-        self.binary_path = os.path.join(self.output, "binary.jpg")
-        
-        #write bianry image to output folder
-        try:
-            cv.imwrite(self.binary_path, self.binary_mtx)
-            print(f'Binary image saved to: {self.binary_path}')            
-        except Exception as e:
-            print(f'Error saving image: {self.binary_path}. Error {e}')
-            return
+        self.save_color_mtx()
+        self.convert_to_gray()
+        self.convert_to_binary()
         self.save_binary_mtx()
-        
+        self.save_binary_jpg()

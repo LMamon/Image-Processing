143e813cdbb1c78a23c5cc6565d9a503d261bce9
LMamon <knit-close.0o@icloud.com>
2025-01-02 17:27:31 -0900

    add: reinitionalize managers on image selection

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 '''main menu provides navigation and options for user'''
 
 import sys
-import ImageManager as img
-import ObjectManager as obj
+import ImageManager
 import RenderManager as rnd
 
     
 
 def main():#main menu prompt
-    img_mgr = img.ImageManager()
-    obj_mgr = obj.ObjectManager()
-    rnd_mgr = rnd.RenderManager()
+    img_mgr = ImageManager.ImageManager() #instanace of image manager
+    rnd_mgr = rnd.RenderManager()    
+    
 
-    choice = input("\t\tWelcome!\n \n1) Choose image with object(s) \n2) Convert image to binary \
-                   \n3) Show centroid of object \n4) Show least and max moment Axis\n0) Exit\n>>")
+    choice = input("\t\tWelcome!\n \n1) Choose image with object(s) \n2) Show binary \
+                   \n3) Show color image \n4) Show centroid of object(s) \
+                   \n5) Show least and max moment Axis\n0) Exit\n>>")
     options = {
-        '1' : img_mgr.get_img, #get user to select image
-        '2' : img_mgr.convert, #threshold> binary>store binary in results
-        #'5' : rnd_mgr.show_color,
-        '3' : obj_mgr.show_centroid,
-        '4' : obj_mgr.show_axis,
+        '1' : img_mgr.get_image, #get user to select image
+        '2' : rnd_mgr.show_binary,
+        '3' : rnd_mgr.show_color,
+        '4' : rnd_mgr.show_centroid,
+        
         '0' : sys.exit
     }
     if choice == '0':

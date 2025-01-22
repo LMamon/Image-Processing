f93d0d2d6db23e234ad73b7a629d5651cc7533ca
LMamon <knit-close.0o@icloud.com>
2025-01-21 18:32:48 -0900

    fix: line/text colors

--- RenderManager.py
+++ RenderManager.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2 as cv
+import math
 
 
 class RenderManager:
@@ -42,7 +43,7 @@ class RenderManager:
 
     def show_centroid(self):
         components = np.load('results/components.npy', allow_pickle=True) #load components from matrix
-        render = self.threshold_jpg.copy()
+        render = cv.cvtColor(self.threshold_jpg, cv.COLOR_GRAY2BGR)
         
         for component in components: #get centroids
             centroid = component['centroid']
@@ -52,14 +53,18 @@ class RenderManager:
 
             #draw centroids on image
             x, y = centroid
-            cv.circle(render, (x, y), 3, color=(0, 0, 255), thickness=1)
-            cv.putText(render, 
-                    f"Centroid: ({x}, {y})", 
-                    (x + 5, y - 5), #offset text for visibility
-                    cv.FONT_HERSHEY_PLAIN, 
-                    .3,
-                    (0,0,255),
-                    1)
+            cv.circle(img= render,
+                      center= (x, y),
+                      radius= 3, 
+                      color=(0, 0, 255), 
+                      thickness=1)
+            cv.putText(img= render, 
+                    text= f"Centroid: ({x}, {y})", 
+                    org= (x + 5, y - 5), #offset text for visibility
+                    fontFace= cv.FONT_HERSHEY_PLAIN, 
+                    fontScale= .3,
+                    color= (0,0,255),
+                    lineType= 1)
         
         valid_objects = [c for c in components if c['centroid'] is not None]
         print(f"Objects detected: {len(valid_objects)}")
@@ -68,3 +73,41 @@ class RenderManager:
         cv.waitKey(0)
         cv.destroyAllWindows()
     
+    def show_axis(self):
+        #load components from matrix
+        components = np.load('results/components.npy', allow_pickle=True)
+        render = cv.cvtColor(self.threshold_jpg, cv.COLOR_GRAY2BGR)
+
+        for component in components:
+            centroid = component['centroid']
+            major_axis = component['major_axis']
+            minor_axis = component['minor_axis']
+            angle = component['angle']
+            if centroid is None:
+                continue
+
+            #draw centroids on image
+            cx, cy = centroid
+            cv.circle(render, (cx, cy), 3, color=(0, 0, 255), thickness=1)
+
+            # Draw major axis
+            half_major = major_axis / 2
+            x1_major = int(cx + half_major * math.cos(angle))
+            y1_major = int(cy + half_major * math.sin(angle))
+            x2_major = int(cx - half_major * math.cos(angle))
+            y2_major = int(cy - half_major * math.sin(angle))
+            cv.line(render, (x1_major, y1_major), (x2_major, y2_major), color= (0, 0, 255), thickness=2)
+
+            # Draw minor axis
+            half_minor = minor_axis / 2
+            x1_minor = int(cx + half_minor * (-math.sin(angle)))
+            y1_minor = int(cy + half_minor * math.cos(angle))
+            x2_minor = int(cx - half_minor * (-math.sin(angle)))
+            y2_minor = int(cy - half_minor * math.cos(angle))
+            cv.line(render, (x1_minor, y1_minor), (x2_minor, y2_minor), color= (255, 0, 0), thickness=1)
+
+        # Show result
+        cv.imshow("Axes", render)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        
